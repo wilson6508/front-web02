362b198c33ebcf2840ad646ac11c13715c1eae7d
wilson6508 <wilson650837@gmail.com>
2021-08-15 18:35:21 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,7 +24,7 @@ export default {
   mounted() {
     // this.pieURL = this.normalURL + this.pieURL;
     this.initializeURL();
-    this.$router.push("/PieChart");
+    // this.$router.push("/PieChart");
   },
   data() {
     return {
