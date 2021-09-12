12d6636f5de9c094cd56a82b410ba66163305938
wilson6508 <wilson650837@gmail.com>
2021-09-12 12:10:52 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
         <b-nav-item href="#" @click="change('LineChart')">折線圖</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="collapse navbar-collapse justify-content-end">
-        <b-nav-item href="https://docs.google.com/forms/d/e/1FAIpQLSciIB-sX4oNoz25sqtvokGIHEu0fgSGlnV77ldIX64B876iGQ/viewform?vc=0&c=0&w=1&flr=0" target="_blank">聯絡開發者</b-nav-item>
+        <b-nav-item href="https://docs.google.com/forms/d/e/1FAIpQLSciIB-sX4oNoz25sqtvokGIHEu0fgSGlnV77ldIX64B876iGQ/viewform?vc=0&c=0&w=1&flr=0" target="_blank">聯絡作者</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
     <div style="padding: 50px">
